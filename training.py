14c0cc9f2133378a1c2d66979a12998f54e7c1d5
Leonbons <leon.bonstrom@gmail.com>
2023-06-23 14:53:29 +0200

    second

--- training.py
+++ training.py
@@ -5,4 +5,26 @@ import numpy as np
 
 import nltk
 from nltk.stem import WordNetLemmatizer
-from tensoerFlow.keras.models import Sequential
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').reads())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!',',', '.']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.tokenize(pattern) #tokenize splittar de olika orden i en mening
+        words.extend(word_list)
+        documents.append((word_list), intent['tag'])
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+print(documents)
